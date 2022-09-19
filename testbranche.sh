19645d21f99a0b146f1b42f105f782d65ddd94d8
PELTIER Dorian <dpeltier@bravo.pedago.ipb.fr>
2022-09-19 15:57:38 +0200

    chenilles

--- testbranche.sh
+++ testbranche.sh
@@ -1,2 +1,3 @@
 Je suis une branche bien touffue de feuilles de séquoya. Le séquoya n'a pas de fruit mais son bois 
-est très nourris.
+est très nourris. Or les chenilles me dérangent beacoup et bouffent sans arrêt mes feuilles
+ ces spèces de gnous !
